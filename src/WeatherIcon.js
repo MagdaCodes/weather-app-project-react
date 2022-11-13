d53218e84cbe6866c3ba989876a12fe28bc5d9a8
MagdaCodes <themsway123@gmail.com>
2022-11-13 18:39:21 +0100

    Added styling

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="blue"
+      color="#234685"
       size={50}
       animate={true}
     />
