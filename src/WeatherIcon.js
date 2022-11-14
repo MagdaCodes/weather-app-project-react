c51cfbd52aab51037418f709803fe3362631e549
MagdaCodes <themsway123@gmail.com>
2022-11-14 10:43:24 +0100

    Added layout for forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#234685"
-      size={50}
+      size={props.size}
       animate={true}
     />
   );
